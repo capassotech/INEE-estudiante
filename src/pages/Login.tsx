c5763e15427438e321641dcf66eb4cf80189971b
FedericoHerreraa <fede.juan.herrera@gmail.com>
2025-08-14 16:59:02 -0300

    mostrando los errores completos abajo del input y separando el authForm en view y controller para mayor legibilidad

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AuthForm from "../components/AuthForm";
+import AuthForm from "../components/AuthForm/AuthFormController";
 
 const Login = () => {
   return <AuthForm isLogin={true} />;
