e88eb175c07fb1da19b284d779818c7846a856c8
marielboher1 <mariel.boher@capasso.tech>
2025-08-19 16:59:14 -0300

    se actualizo el puerto del backend

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider, signInWithCustomToken, signOut, signInWith
 import { auth } from "../../config/firebase-client";
 import { RegisterData, LoginData, AuthResponse, UserProfile } from "../types/types";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://inee-backend.onrender.com";
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
