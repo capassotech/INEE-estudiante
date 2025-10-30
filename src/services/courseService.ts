7b1a45a5aba889ff3cd6ba091c95c02da6af9dd8
marielboher1 <mariel.boher@capasso.tech>
2025-10-30 08:47:45 -0300

    urls apuntando a prod

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { auth } from "../../config/firebase-client";
 import { Course } from "@/types/types";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "https://inee-backend.onrender.com";
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
