79c70e18b6674df7cd36b1ac6c6a2748761ba146
eliascapasso <elias_capasso@live.com>
2026-01-06 17:21:23 -0300

    deploy automatico

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported, Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAZDT5DM68-9qYH23HdKAsOTaV_qCAPEiw",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "inee-admin.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "inee-admin",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "inee-admin.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "746757753608",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:746757753608:web:c63f0a8d2d60f10fefbabd",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-PH54SWBE8V",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
