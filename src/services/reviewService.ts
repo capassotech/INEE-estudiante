e2dfb5f7abdfad49fec612c45f33c4f764ce9bd8
marielboher1 <mariel.boher@capasso.tech>
2026-01-12 13:20:54 -0300

    cambio de local

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { auth } from "../../config/firebase-client";
 
 const API_BASE_URL =
-  // import.meta.env.VITE_API_URL || "https://inee-backend.onrender.com";
-  "http://localhost:3000";
+  import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}`,
